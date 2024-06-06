d1f2b20afdc7cb017e4d39f6681bb51811f6f96c
Kostadin <eftimovk99@gmail.com>
2024-06-06 12:10:24 +0200

    Initial GUI: measurement start and data plot

---
+++ Biosensor GUI/Biosensor GUI/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Biosensor_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            StartBtn = new Button();
+            dataPlot = new ScottPlot.WinForms.FormsPlot();
+            button2 = new Button();
+            textBoxLog = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            StartBtn.Font = new Font("Arial", 14F);
+            StartBtn.Location = new Point(819, 132);
+            StartBtn.Name = "StartBtn";
+            StartBtn.Size = new Size(196, 71);
+            StartBtn.TabIndex = 0;
+            StartBtn.Text = "Start\r\nmeasurement";
+            StartBtn.UseVisualStyleBackColor = true;
+            StartBtn.Click += StartBtn_Click;
+            // 
+            // dataPlot
+            // 
+            dataPlot.DisplayScale = 1.25F;
+            dataPlot.Location = new Point(23, 132);
+            dataPlot.Name = "dataPlot";
+            dataPlot.Size = new Size(740, 491);
+            dataPlot.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(877, 243);
+            button2.Name = "button2";
+            button2.Size = new Size(87, 43);
+            button2.TabIndex = 2;
+            button2.Text = "stop";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLog
+            // 
+            textBoxLog.Location = new Point(819, 349);
+            textBoxLog.Multiline = true;
+            textBoxLog.Name = "textBoxLog";
+            textBoxLog.Size = new Size(212, 212);
+            textBoxLog.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Logo_without_name_framed;
+            pictureBox1.Location = new Point(346, 32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(129, 94);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1064, 702);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBoxLog);
+            Controls.Add(button2);
+            Controls.Add(dataPlot);
+            Controls.Add(StartBtn);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button StartBtn;
+        private ScottPlot.WinForms.FormsPlot dataPlot;
+        private Button button2;
+        private TextBox textBoxLog;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Biosensor GUI/Biosensor GUI/Form1.cs
@@ -0,0 +1,156 @@
+using ScottPlot;
+using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Biosensor_GUI
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+        private int dataPointsMax = 4000;   // max. number of datapoints in the plot
+        private int[] dataX;               // X-axis data (time or sample number)
+        private int[] dataY;               // Y-axis data (received values)
+        private int dataYmin = 32800;
+        private int dataYmax = 32000;
+        private ScottPlot.Plottables.DataStreamer streamerPlot;
+        readonly System.Windows.Forms.Timer UpdatePlotTimer = new() { Interval = 50, Enabled = true };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxLog.AppendText("-----Log-----" + Environment.NewLine + Environment.NewLine);
+            dataX = new int[dataPointsMax];
+            dataY = new int[dataPointsMax];
+
+            for (int i = 0; i < dataPointsMax; i++)
+            {
+                dataX[i] = i;
+            }
+
+            InitializeSerialPort();
+            InitializePlot();
+        }
+
+        /*
+         * Initializations
+        */
+        private void InitializeSerialPort()
+        {
+            string portName = "COM21"; // TODO: find the right port automatically
+            serialPort = new SerialPort(portName, 115200);
+            // TODO: write port settings explicitly (baud rate, data bits, parity, ...)
+
+            // serialPort.DataReceived += DataReceivedHandler; // Subscribe to the DataReceived event
+
+            try
+            {
+                serialPort.Open();
+                textBoxLog.AppendText("Connected to " + portName + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening serial port: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void InitializePlot()
+        {
+            UpdatePlotTimer.Tick += UpdatePlotTimer_Tick;
+            streamerPlot = dataPlot.Plot.Add.DataStreamer(dataPointsMax);
+            // dataPlot.Plot.Add.Scatter(dataX, dataY);
+            dataPlot.Plot.Axes.SetLimits(0, dataPointsMax, 0, 100);
+            dataPlot.Plot.Axes.Bottom.Label.Text = "Data Points";
+            dataPlot.Plot.Axes.Left.Label.Text = "ADC code";
+            dataPlot.Refresh();
+        }
+
+        private void UpdatePlotTimer_Tick(object sender, EventArgs e)
+        {
+            if (streamerPlot.CountTotal > dataPointsMax) // start shifting plot when we reach the end of the x-axis
+                streamerPlot.ViewScrollLeft();
+            dataPlot.Refresh();
+        }
+
+        /*
+         * Data handling (receive and plot)
+        */
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            while (serialPort != null && serialPort.IsOpen)
+            {
+                string data = serialPort.ReadLine();
+
+                // Update the UI in a thread-safe manner
+                Invoke(() =>
+                {
+                    // Log that data is currently being received
+                    string receiveLog = "Receiving data...";
+                    if (textBoxLog.Lines.Any())
+                    {
+                        string lastLine = textBoxLog.Lines[textBoxLog.Lines.Length - 2];
+                        if (lastLine != receiveLog)
+                            textBoxLog.AppendText(receiveLog + Environment.NewLine);
+                    }
+
+                    UpdateDataStreamPlot(data);
+
+                });
+            }
+        }
+        
+        private void UpdateDataStreamPlot(string data)
+        {
+            data = data.TrimEnd('\r', '\n');
+
+            int newValue;
+            bool res = Int32.TryParse(data, out newValue);
+
+            if (res)
+            {
+                streamerPlot.Add(newValue);
+                dataPlot.Plot.Axes.SetLimits(0, dataPointsMax, dataYmin, dataYmax);
+            }
+        }
+
+        /*
+         * Button clicks
+        */
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                byte cmdToSend = (byte)(1); // one means 'start-measurement'
+
+                try
+                {
+                    serialPort.Write(new byte[] { cmdToSend }, 0, 1);
+                    textBoxLog.AppendText("Sent START cmd" + Environment.NewLine);
+                    var t = new Thread(() => DataReceivedHandler(null, null));  // do serial port read in separate thread
+                    t.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error sending a command: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Serial port is not open.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+    }
+}
