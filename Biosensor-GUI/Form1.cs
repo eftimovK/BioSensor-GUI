7242754bf85be78fd56507d123a746b89877e115
Stefan.K <stefan.kreher@rwth-aachen.de>
2024-06-29 11:55:13 +0200

    Start Sequence buttom

--- Biosensor-GUI/Form1.cs
+++ Biosensor-GUI/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -32,9 +33,29 @@ namespace Biosensor_GUI
          */
         private void start_meas_Click(object sender, EventArgs e)
         {
-            textBoxLog.AppendText("Data receive" + Environment.NewLine);
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                byte cmdToSend = (byte)(1); // one means 'start-measurement'
+
+                try
+                {
+                    serialPort.Write(new byte[] { cmdToSend }, 0, 1);
+                    textBoxLog.AppendText("START cmd sent" + Environment.NewLine);
+
+                    //enable or disable buttons
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error sending a command: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Serial port is not open.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
+        //Select Com through selection box --> search for and select available Ports
         private void comSelBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (comSelBox.SelectedItem != null)
@@ -60,6 +81,7 @@ namespace Biosensor_GUI
             }
         }
 
+        //Refresh the com selection Box  
         private void refrBut_Click(object sender, EventArgs e)
         {
             comSelBox.Items.Clear();
